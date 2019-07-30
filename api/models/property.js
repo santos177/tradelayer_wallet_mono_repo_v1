509387039fdafe753895f2036a2b61080bc8ce91
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-07-30 16:58:56 -0400

    update property model

--- api/models/property.js
+++ api/models/property.js
@@ -3,10 +3,19 @@ const Sequelize = require("sequelize");
 module.exports = db => {
   return db.define("property", {
     name:{
-      type: Sequelize.STRING, unique: true, alloNull: false
+      type: Sequelize.STRING, unique: true, allowNull: false
     },
     prop_id: {
-      type: Sequelize.STRING, unique: true, alloNull: false
-    }
+      type: Sequelize.STRING, unique: true, allowNull: false
+    },
+    data:Sequelize.STRING,
+    url :Sequelize.STRING,  
+    category: Sequelize.STRING,
+    subcategory: Sequelize.STRING,
+    creationTxId: Sequelize.STRING,
+    totalTokens: Sequelize.INTEGER,
+    creationBlock: Sequelize.INTEGER,
+    divisible: Sequelize.BOOLEAN, 
+    fixedIssuance: Sequelize.BOOLEAN
   });
 };
