aa25768dab65ab87103cfa8ce363d7e0f48a6fcd
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-07-30 19:41:31 -0400

    add property option to orderbook

--- api/routes/orderbook.js
+++ api/routes/orderbook.js
@@ -5,37 +5,47 @@ var datadir = config.TLDATADIR;
 const express = require("express");
 const orderbookRouter = express.Router();
 
-// add property option as well
 orderbookRouter.get("/", (req, res) => {
   const { omniClient } = req;
-  let { contractID } = req.query;
-  omniClient.cmd("tl_getcontract_orderbook", contractID, 1, function whenOK(
-    err,
-    buyResp,
-    resHeaders
-  ) {
-    if (err === null) {
-      buyObj = JSON.stringify(buyResp, null, "\t");
-      omniClient.cmd("tl_getcontract_orderbook", contractID, 2, function whenOK(
-        err2,
-        sellResp,
-        resHeaders
-      ) {
-        if (err2 === null) {
-          sellObj = JSON.stringify(sellResp, null, "\t");
-          jsonTxt = "[" + sellObj + "," + buyObj + "]";
-          var jsonObj = JSON.parse(jsonTxt);
-          res.send(jsonObj);
-        } else {
-          console.log("something went wrong is orderbook sell ", err2);
-          res.send(err2);
-        }
-      });
-    } else {
-      console.log("something went wtong in orderbook buy ", err);
-      res.send(err);
-    }
-  });
+  let { contractID, propertyID } = req.query;
+	contractID = +contractID;
+	// of note: the TL api needs the prop_id to be a Number, but doesn't care about the contractID
+	propertyID = +propertyID;
+  if (propertyID !== undefined) {
+    omniClient.cmd("tl_getorderbook", +propertyID, (err, result) => {
+      if (err) console.warn(err);
+      res.send(result);
+    });
+  } else {
+    omniClient.cmd("tl_getcontract_orderbook", contractID, 1, function whenOK(
+      err,
+      buyResp,
+      resHeaders
+    ) {
+      if (err === null) {
+        buyObj = JSON.stringify(buyResp, null, "\t");
+        omniClient.cmd(
+          "tl_getcontract_orderbook",
+          contractID,
+          2,
+          function whenOK(err2, sellResp, resHeaders) {
+            if (err2 === null) {
+              sellObj = JSON.stringify(sellResp, null, "\t");
+              jsonTxt = "[" + sellObj + "," + buyObj + "]";
+              var jsonObj = JSON.parse(jsonTxt);
+              res.send(jsonObj);
+            } else {
+              console.log("something went wrong is orderbook sell ", err2);
+              res.send(err2);
+            }
+          }
+        );
+      } else {
+        console.log("something went wtong in orderbook buy ", err);
+        res.send(err);
+      }
+    });
+  }
 });
 
 module.exports = orderbookRouter;
