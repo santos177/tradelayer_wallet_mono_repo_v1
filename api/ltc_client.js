e76661e2fb715fe7e659e491e47262e69dff0ff7
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-07-30 16:23:12 -0400

    add port as config option

--- api/ltc_client.js
+++ api/ltc_client.js
@@ -1,8 +1,9 @@
 var litecoin = require('litecoin');
+const config = require('./config')
 
 module.exports = new litecoin.Client({
     host: 'localhost',
-    port: 9332,
+    port: config.TLPORT,
     user: "pepejandro",
     pass: "pepecash",
     ssl: false/* true, */
