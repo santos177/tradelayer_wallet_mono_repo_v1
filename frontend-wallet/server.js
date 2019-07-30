4584c831f038bc31bb96099d0c0f7159249b345a
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-07-30 11:06:03 -0400

    change default port

--- frontend-wallet/server.js
+++ frontend-wallet/server.js
@@ -7,7 +7,7 @@ var serveStatic = require('serve-static');
 app =express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5001;
+var port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log('server started' + port);
