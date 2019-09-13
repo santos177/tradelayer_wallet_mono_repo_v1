6a5a07a81b261c0c69ae06553a8c36ad41c30f16
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-09-13 11:05:34 -0400

    remove account module and related stuff

--- frontend-wallet/src/store/index.js
+++ frontend-wallet/src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { alert } from './alert.module'
-import { account } from './account.module'
 import { auth } from './auth.module'
 import { orderbook } from './orderbook.module'
 import { contracts } from './contracts.module'
@@ -15,7 +14,6 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
   modules: {
     alert,
-    account,
     auth,
     orderbook,
     contracts,
