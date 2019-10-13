75324b862f7003301ae5bf54978ee6fb3fdbc6a3
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-10-13 10:41:46 -0400

    add get routes

--- api/routes/position.js
+++ api/routes/position.js
@@ -28,4 +28,30 @@ positionRouter.post("/postFullPositions", (req, res) => {
   );
 });
 
+positionRouter.get('/getVolume', (req, res)=>{
+  const {contractID, blocks} = req.query;
+  const {omniClient} = req; 
+  omniClient.cmd('tl_getvolume', contractID, blocks, (err, volume )=>{
+    if(err){
+      res.send(err.toString())
+    } else {
+      res.send(volume)
+
+    }
+  })
+})
+
+
+positionRouter.get('/getQuotes', (req, res)=>{
+  const {contractID, trustRating} = req.query;
+  const {omniClient} = req; 
+  omniClient.cmd('tl_getquotes', contractID, trustRating, (err, quotes )=>{
+    if(err){
+      res.send(err.toString())
+    } else {
+      res.send(quotes)
+    }
+  })
+})
+
 module.exports = positionRouter
