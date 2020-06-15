13b81c12f75b64cfad17bd12544487fa17826d6c
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2020-06-15 19:00:07 -0400

    Updated Routes to Reflect Native+Oracle RPCs

--- api/routes/property.js
+++ api/routes/property.js
@@ -8,8 +8,19 @@ propertyRouter.get('/listproperties', (req,res)=>{
         res.send(properties)
     })
 })
-propertyRouter.get('/listContracts', (req,res)=>{
-    req.omniClient.cmd('tl_listcontracts', (err, contracts)=>{
+
+propertyRouter.get('/listNatives', (req,res)=>{
+    req.omniClient.cmd('tl_list_natives', (err, contracts)=>{
+        if(err){
+            res.send(err.toString())
+        } else {
+            res.send(contracts)
+        }       
+    })
+})
+
+propertyRouter.get('/listOracleContracts', (req,res)=>{
+    req.omniClient.cmd('tl_list_oracles', (err, contracts)=>{
         if(err){
             res.send(err.toString())
         } else {
@@ -22,16 +33,6 @@ propertyRouter.get('/getproperty/:id', (req, res)=>{
     let {id} = req.params
     id = +id    
     req.omniClient.cmd('tl_getproperty', id, (err, properties)=>{
-        console.log(err, properties);
-        
-        res.send(properties)
-    }) 
-})
-
-propertyRouter.get('getcontractinfo/:id', (req, res)=>{
-    let {id} = req.params
-    id = +id    
-    req.omniClient.cmd('tl_getcontractinfo', id, (err, properties)=>{
         console.log(err, properties);
         
         res.send(properties)
