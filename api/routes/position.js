003b7b6e81feeab3c91f2acb97a0bfa6d530ccac
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2020-06-22 19:33:53 -0400

    Cut the vestigial commads to move to marketdata

--- api/routes/position.js
+++ api/routes/position.js
@@ -27,43 +27,5 @@ positionRouter.get("/getFullPosition", (req, res) => {
     }
   );
 });
-//deprecated until associated RPC is implemented, also this should go under its own route with other trade history data.
-/*positionRouter.get('/getContractVolume', (req, res)=>{
-  const {contractID, startblock, endblock} = req.query;
-  const {omniClient} = req; 
-  omniClient.cmd('tl_getvolume', contractID, startblock, endblock, (err, volume )=>{
-    if(err){
-      res.send(err.toString())
-    } else {
-      res.send(volume)
-
-    }
-  })
-})*/
-
-
-positionRouter.get('/getQuotes', (req, res)=>{
-  const {contractID, trustRating} = req.query;
-  const {omniClient} = req; 
-  omniClient.cmd('tl_getquotes', contractID, trustRating, (err, quotes )=>{
-    if(err){
-      res.send(err.toString())
-    } else {
-      res.send(quotes)
-    }
-  })
-})
-
-positionRouter.get('/getInsurance', (req, res)=>{
-  const {contractID} = req.query;
-  const {omniClient} = req; 
-  omniClient.cmd('tl_getinsurance', contractID, (err, insurance )=>{
-    if(err){
-      res.send(err.toString())
-    } else {
-      res.send(insurance)
-    }
-  })
-})
 
 module.exports = positionRouter
