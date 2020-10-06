ae0d6f87bae566b6fa335a492679a50ac8199c16
Valentin <valiopld@gmail.com>
2020-10-06 17:09:52 +0300

    setup dev env configs

--- frontend-wallet/config/dev.env.js
+++ frontend-wallet/config/dev.env.js
@@ -1,10 +1,12 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-const baseUrl = "http://192.155.93.12";  
+const baseUrl = "localhost";
+const basePort = 3002;
+const socketPort = 75
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: `"${baseUrl}:76/api"`,
-  SOCKET_URL:`"${baseUrl}:76"`
+  API_URL: `"${baseUrl}:${basePort}/api"`,
+  SOCKET_URL:`"${baseUrl}:${socketPort}"`
 })
