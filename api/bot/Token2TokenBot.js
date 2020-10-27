d4874c6fc7364f5b8ada4cda7124ec20d6d2515d
Valentin <valiopld@gmail.com>
2020-10-27 21:26:48 +0200

    t2tBot update

--- api/bot/Token2TokenBot.js
+++ api/bot/Token2TokenBot.js
@@ -3,16 +3,16 @@ var tl = require('./TradeLayerRPCAPI.js').tl
 var loops = 0
 var property1 = 12 // add property 1
 var property2 = 13 // add property 2
-var address1 = "QNQGyQs75G2wrdkVhQAVztoU9Ma6EQe1a8" // add address 1
-var address2 = "QbbqvDj2bJkeZAu4yWBuQejDd86bWHtbXh" // add address 2 
+var address1 = "mo3MiLeWQzYxrPugKzyB9bEJKGbEzLcs91" // add address 1
+var address2 = "mo3MiLeWQzYxrPugKzyB9bEJKGbEzLcs91" // add address 2 
 var deleteCount = 30
-var intervalMs = 30000
+var intervalMs = 2000
 
 function tradeBot(){
     setInterval(function(){
             loops +=1
             var amount1 =  Math.random()>=0.5 ? 0.01 : 0.025
-            var amount2 = Math.random()*0.02
+            var amount2 = parseFloat((Math.random()*0.02).toFixed(4))
             var address = loops%2==0 ? address1 : address2
             var params = {
                 address,
@@ -24,7 +24,7 @@ function tradeBot(){
             console.log(`loop number: ${loops}`);
             tl.sendtrade(params,function(data, err){
                 if(err) {
-                    console.log("There is an error with Trade!!!\n", err)
+                    console.log("There is an error with Trade!!!\n", params, err)
                 } else {
                     console.log("Successful Trade: \n", data)
                 }
