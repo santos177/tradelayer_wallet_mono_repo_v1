ab367b3e40fad21f40b8a523d6eefc711fb01cee
ogadimma osuji <ogadimma.osuji@mkobo.com.ng>
2020-11-01 08:29:44 +0100

    modified the block transaction router

--- api/routes/block-transactions.js
+++ api/routes/block-transactions.js
@@ -5,65 +5,18 @@ const { redisClient } = require('../redis_client');
 const blockTxnModule = require('./modules/blockTxn-module');
 const omniClient = require('../ltc_client');
 
-blockTransactionsRouter.get('/:blockhash', (req, res) => {
+blockTransactionsRouter.get('/:block', (req, res) => {
 
-    const { blockhash } = req.params;
-    // const blockTxnRedisKey = 'blockTxnRedisKey';
-
-    // blockTransactionsModule(blockhash)
-    // .then(() => {})
-    // .then(() => {
-
-    //     redisClient.get(blockTxnRedisKey, (err, result) => {
-
-    //         // if(result && JSON.parse(result).length !== 0) {
-    //         //     console.log('result_xxx: ', result)
-    //         //     return res.json({
-    //         //         isResult: true,
-    //         //         data: JSON.parse(result)
-    //         //     })
-    //         // }
-    //         // return res.json({
-    //         //     isResult: false,
-    //         //     data: [],
-    //         // })
-
-    //         blockTxnModule(result)
-    //         .then(response => {
-    //             res.json(response);
-    //         })
-    //         .catch(error => {
-    //             res.json(error);
-    //         })
-    //     })
-    // })
-    // .catch(() => {
-    //     return res.json({
-    //         isResult: false,
-    //         data: [],
-    //     })
-    // })
-    
-
-
-    // redisClient.get(blockTxnRedisKey, (err, blockTransaction) => {
-    //     if((JSON.parse(blockTransaction).length < 1) && (JSON.parse(blockTransaction).length > -1)) {
-            
-    //         let blockTransactionArr = JSON.parse(blockTransaction);
-    //         console.log('blockTransactionArr: ', blockTransactionArr)
-            
-    //     }
-    // });
+    const { block } = req.params;
 
     const blockTransactionArr = [];
 
-    omniClient.cmd('getblock', blockhash, (err, block) => {
+    omniClient.cmd('tl_listblocktransactions', block, (err, blockTransactions) => {
 
-        if(block) {
-            const { tx } = block;
-            console.log('tx: ', tx)
-            tx.map((transaction, index) => {
-                omniClient.cmd('getrawtransaction', transaction, true, (err, blockTransaction) => {
+        if(blockTransactions) {
+            console.log('block transactions: ', blockTransactions)
+            blockTransactions.map((blockTransaction, index) => {
+                omniClient.cmd('tl_listtransactions', blockTransaction, true, (err, blockTransaction) => {
 
                     if(blockTransaction) {
                         const blockTransactionObj = {
@@ -76,16 +29,6 @@ blockTransactionsRouter.get('/:blockhash', (req, res) => {
             })
 
             setTimeout(() => {
-                // if(blockTransactionArr && blockTransactionArr > 0) {
-                //     res.json({
-                //         isResult: true,
-                //         data: blockTransactionArr
-                //     })
-                // }
-                // res.json({
-                //     isResult: false,
-                //     data: blockTransactionArr
-                // })
                 res.json({
                     isResult: true,
                     data: blockTransactionArr
