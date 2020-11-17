ea511d889dc0f6b65e9e119d17006c8475a7c96e
Valentin <valiopld@gmail.com>
2020-11-17 20:26:07 +0200

    Update dropdowns cotracts list in most-left button menu

--- frontend-wallet/src/components/Navigation.vue
+++ frontend-wallet/src/components/Navigation.vue
@@ -14,20 +14,45 @@
           <span class="md-label">Trade Channel Manager </span>
         </router-link>
       </div>
-  <div class='divider' />
 
-      [LTC Pairs]
-     <md-list class='pairs-list'>
-      <md-list-item class='pair-item'>LTC/dUSD</md-list-item>
-      <md-list-item class='pair-item'>LTC/ALL</md-list-item>
-      <md-list-item class='pair-item'>LTC/USDL</md-list-item>
-      <md-list-item class='pair-item'>LTC/sLTC</md-list-item>
-    </md-list>
-  <div class='divider' />
-    [Oracle Contracts]
-  <div class='divider' />
-    [Native Contracts]
+    <div class='custom-dropdown'>
+      <div class='dropdown-head' @click="toggleDropdown(1)"> LTC Pairs </div>
+      <div class='dropdown-body'>
+        <div 
+        class='dropdown-item' 
+        v-bind:class="{ active: openedDropdown === 1 }"
+        v-for="(contract,index) in pairContracts"
+        :key='index'
+        >{{contract}}</div>
+      </div>
     </div>
+
+      <div class='custom-dropdown'>
+      <div class='dropdown-head'  @click="toggleDropdown(2)">Oracle Contracts</div>
+      <div class='dropdown-body'>
+        <div 
+          class='dropdown-item' 
+          v-bind:class="{ active: openedDropdown === 2 }"
+          v-for="(contract,index) in oracleContracts"
+          :key='index'
+          >{{contract}}</div>
+      </div>
+    </div>
+
+    <div class='custom-dropdown'>
+      <div class='dropdown-head' @click="toggleDropdown(3)">Native Contracts</div>
+      <div class='dropdown-body'>
+        <div 
+          class='dropdown-item' 
+          v-bind:class="{ active: openedDropdown === 3 }"
+          v-for="(contract ,index) in nativeContracts"
+          :key='index'
+          >{{contract}}</div>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
@@ -37,10 +62,35 @@ export default {
   name: 'Navigation',
   components: {},
   data () {
-    return {}
+    return {
+      openedDropdown: 0,
+      pairContracts: ["LTC/dUSD", "LTC/ALL", "LTC/USDL", "LTC/sLTC"],
+      oracleContracts: [
+        "LTC/USD-Oracle-PERP", 
+        "LTC/USD-Oracle-DEC20", 
+        "LTC/USD-Oracle-MAR21", 
+        "BTC/USD-Oracle-PERP", 
+        "BTC/USD-Oracle-DEC20", 
+        "BTC/USD-Oracle-MAR21",
+        "LTC/BTC-Oracle-PERP",
+        "ALL/USD-Oracle-PERP",
+        "ALL/LTC-Oracle-PERP"
+        ],
+      nativeContracts: [
+        "ALL/LTC-Native-PERP",
+        "LTC/USD-Native-PERP",
+        "LTC/EUR-Native-PERP",
+        "LTC/JPY-Native-PERP",
+        "LTC/CNY-Native-PERP",
+      ]
+    }
   },
   computed: {},
-  methods: {},
+  methods: {
+    toggleDropdown(id) {
+      this.openedDropdown = this.openedDropdown === id ? 0 : id;
+    }
+  },
   mounted () {
 }
 }
@@ -77,9 +127,51 @@ export default {
   padding: 0;
   background: rgb(41, 41, 43);
   margin: 1px 0;
+  height: 0;
+  overflow: hidden;
+  transition: all 0.5s;
 }
 .pair-item:hover {
   filter: invert(100%);
   cursor: pointer;
+}
+.head {
+    background: rgb(41, 41, 43);
+    padding: 0.5rem;
+}
+.head:hover{
+  cursor: pointer;
+}
+.head:hover + .pairs-list .pair-item {
+  height: 3rem;
+}
+
+
+.dropdown-head {
+  padding: 1rem;
+  background: #367588;
+  color: black;
+  font-size: 1rem;
+  cursor: pointer;
+  border-bottom: 1px solid black;
+  border-top: 1px solid black
+}
+.dropdown-head:hover {
+  cursor: pointer;
+}
+.dropdown-item {
+  background: rgb(41, 41, 43);
+  height: 0rem;
+  overflow: hidden;
+  transition: all 0.5s;
+}
+
+.dropdown-item:hover {
+  filter: invert(100%);
+  cursor: pointer;
+}
+.active {
+  padding: 1rem;
+  height: 3rem;
 }
 </style>
