3aa455bc0de7085403ac71200ab2fb1479183eb3
Valentin <valiopld@gmail.com>
2020-12-18 00:10:57 +0200

    Update sockets listen/receive handshake

--- api/bot/bot_listener.js
+++ api/bot/bot_listener.js
@@ -1,57 +1,63 @@
 const PORT = 9876;
 const listener = require('socket.io')(PORT);
-const tl = require('./TradeLayerRPCAPI').tl
+const tl = require('./TradeLayerRPCAPI').tl 
+
+// var client = tl.init('username', 'passwrod', '', true)
+const tokenAddress = "QNQGyQs75G2wrdkVhQAVztoU9Ma6EQe1a8"
+const propertyId = 4
+const propertyId2 = 5
+const amount = "10";
+
+var channelPubkeyListen = ""
+var channelPubKeyReceive = ""
+var channelAddressListen = ""
+var channelMultisig = ""
 
 listener.on('connection', (io) => {
     console.log(`New Connection! ID: ${io.id}`)
-    // code here 
-
-    let dataSub = {
-    "jsonrpc": "2.0",
-     "method": "public/get_index_price",
-     "id": 42,
-     "params": {
-        "index_name": "btc_usd"}
-    };
 
     let channelComponent = ''
-    let myChannelPubkey = ''
 
-    tl.getnewaddress(null, (data) => {
-            channelComponent = data
-        tl.validateaddress(data, (d) => {
-            myChannelPubkey = d.scriptPubKey
-            io.emit('channelPubKey', myChannelPubkey)
-
+    tl.getNewAddress(null, (data) => {
+        channelAddressListen = data
+        tl.validateAddress(data, (d) => {
+            channelPubkeyListen = d.pubkey
+            io.emit('channelPubKey', channelPubkeyListen)
         })
     });
-    io.emit('dataSub', dataSub)
-
-
-    io.on('deribitJSON', (data) => {
-        BTCUSD = data.result.index_price
-    })
-
-    io.on('pubKey', (data) => {
-        tl.addmultisigaddress(2, [data, channelSingleSigAddress], (data) => {
-            myChannelMultisig = data
-            const multisig = {
+    
+    io.on('channelPubKey', (channelPubkey) => {
+        channelPubKeyReceive = channelPubkey
+        tl.addMultisigAddress(2, [channelPubkeyListen, channelPubKeyReceive], (data) => {
+            channelMultisig = data
+            const multySigData = {
                 'multisig': data,
-                'pubKeyUsed': myChannelPubKey
+                'pubKeyUsed': channelPubkeyListen
             }
-            io.emit('multisig', multisig)
-        })
-    })
 
-    io.on('multisig', (data) => {
-        shouldCoSign(data, (sign, data) => {
-            if (sign === true) {
-                tl.simplesign(data, (data) => {
-                    io.emit('something', data)
-                })
-            }
-        })
-    })
-    //end code here 
-})
+            io.emit('multisig', multySigData)
 
+            legitMultisig(multySigData, (legit) => {
+                if (legit == true) {
+                    channelMultisig = multySigData
+                    const multiSigAddress = multySigData.multisig.address
+                    tl.commitToChannel(tokenAddress, multiSigAddress, propertyId2, amount, function(data){
+                        console.log({data})
+                        return
+                     })
+                } else { 
+                    return console.log('The client tried to scam with a bad multisig')
+                }
+            })
+        })
+        
+
+    })
+});
+
+function legitMultisig(e, cb){
+    tl.addMultisigAddress(2, [channelPubKeyListen, channelPubKeyReceive], function(data){
+        const legit = data.reedemScript == e.multisig.reedemScript ? true : false
+            return cb(legit)
+    })
+}
