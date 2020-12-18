edc8f711fb7f5b0e8fc0155a6eeeb4a2b3d4dac4
Ogadimma Osuji <orgmatter@gmail.com>
2020-12-18 08:21:42 +0000

    bug fixes v1

--- api/jobs/find-new-blocks.js
+++ api/jobs/find-new-blocks.js
@@ -6,6 +6,9 @@ const { redisClient } = require('../redis_client');
 const findNewBlocks = () => {
 
     const getInfoArr = [];
+    var valueArr = [{}, 0, ''];
+   // var propertyId = '';
+    const rate_usd = 1.0;
 
     // first check if getInfo exist on redis
     const getInfoRedisKey = 'getInfo';
@@ -19,8 +22,6 @@ const findNewBlocks = () => {
                 console.log(getInfo)
                 const { block } = JSON.parse(getInfo);
                 const oldBlocks = block;
-                var valueArr = [0, [0]];
-                const rate_usd = 1.0;
 
                 omniClient.cmd('tl_getinfo', (err, response) => {
     
@@ -29,78 +30,111 @@ const findNewBlocks = () => {
                         const { block } = response;
                         const newBlocks = block;
                         const blocksDiff = (newBlocks - oldBlocks);
-                        var oldBlock = oldBlocks;
     
                         // get block hash using the loop
                         for(i = 0; i < blocksDiff; i++) {
-                            oldBlock += i
+                            console.log('blocksDiff: ', blocksDiff)
+                            console.log('i: ', i)
+                            var hashBlock = (oldBlocks + i);
+                            console.log('hashBlock: ', hashBlock)
     
-                            omniClient.cmd('getblockhash', (oldBlock), (err, getBlockHash) => {
+                            omniClient.cmd('tl_listblocktransactions', hashBlock, (err, blockTransactions) => {
+
+                                console.log('oldBlocks: ', hashBlock)
+                                console.log('blockTransactions: ', blockTransactions)
+                                if(blockTransactions.length >= 1) {
+
+                                    console.log('block transaction length: ', blockTransactions.length)
+
+
+                                    omniClient.cmd('getblockhash', hashBlock, (err, getBlockHash) => {
     
-                                if(getBlockHash) {
-
-                                    omniClient.cmd('getblock', getBlockHash, (err, blocksObj) => {
-                                        
-                                        if(blocksObj) {
-                                            console.log('blocksObj', blocksObj)
-
-                                            omniClient.cmd('tl_listblocktransactions', oldBlock, (err, blockTransactions) => {
-
-                                                if(blockTransactions) {
-
+                                        if(getBlockHash) {
+        
+                                            omniClient.cmd('getblock', getBlockHash, (err, blocksObj) => {
+        
+                                                if(blocksObj) {
+                                                    console.log('blocksObj', blocksObj)
+        
                                                     blockTransactions.map((blockTransaction, index) => {
+                                                        console.log('blockTransaction: ', blockTransaction)
+                
+                                                        omniClient.cmd('tl_gettransaction', blockTransaction, (err, transaction) => {
+                                                            console.log('transaction: ', transaction)
+                                                            if(transaction) {
 
-                                                        omniClient.cmd('tl_gettransaction', blockTransaction, (err, blockTransaction) => {
-
-                                                            if(blockTransaction) {
-                                                                var value = {
-                                                                    details: {
-                                                                        [blockTransaction.propertyId]: {
-                                                                            flags: null,
-                                                                            invalid: 0,
-                                                                            name: blockTransaction.propertyname,
-                                                                            rate_usd,
-                                                                            tx_count: blockTransactions.length,
-                                                                            value_usd_rounded: "",
-                                                                            volume: "",
+                                                                console.log('transaction: ', transaction)
+                
+                                                                omniClient.cmd('tl_getproperty', transaction.propertyId, (err, propertyData) => {
+                
+                                                                    if(propertyData) {
+                
+                
+                                                                        var value = {
+                                                                            details: {
+                                                                                [transaction.propertyId]: {
+                                                                                    flags: null,
+                                                                                    invalid: 0,
+                                                                                    name: transaction.propertyname,
+                                                                                    rate_usd,
+                                                                                    tx_count: blockTransactions.length,
+                                                                                    value_usd_rounded: "",
+                                                                                    volume: "",
+                                                                                }
+                                                                            },
+                                                                            total_usd: "",
+                                                                            value_24hr: propertyData['last 24h Token volume'],
                                                                         }
-                                                                    },
-                                                                    total_usd: "",
-                                                                    value_24hr: "",
-                                                                }
+                                                                        valueArr[0] = value;
+                                                                        valueArr[1] += parseFloat(transaction.amount !== undefined ? transaction.amount: 0);
+                                                                        valueArr[2] = transaction.propertyId;
 
-                                                                valueArr[0] = value;
-                                                                valueArr[1] += blockTransaction.amount;
+                                                                        setTimeout(() => {
+                                                                          console.log('value arr: ', valueArr)  
+                                                                        }, 600)
+                                                                    }
+                                                                })
+                                                            }else{
+                                                                console.log('no transaction: ', err)
                                                             }
                                                         })
                                                     })
+
+                                                    setTimeout(() => {
+
+                                                        console.log('value array: ', {...valueArr[0]});
+                                                        console.log('property id: ', valueArr[2]);
+                                                        const blocksInfoObj = {
+                                                            block: blocksObj.height,
+                                                            block_hash: blocksObj.hash,
+                                                            timestamp: blocksObj.time,
+                                                            omni_tx_count: valueArr[2] !== ''? valueArr[0]['details'][valueArr[2]].tx_count : '',
+                                                            value: {
+                                                                ...valueArr[0],
+                                                                details: valueArr[2] !== ''?  {
+                                                                    [valueArr[2]]: {
+                                                                        ...valueArr[0]['details'][valueArr[2]],
+                                                                        value_usd_rounded: Math.ceil((valueArr[1] * parseFloat(rate_usd))),
+                                                                        volume: valueArr[1],
+                                                                        total_usd: Math.ceil((valueArr[1] * parseFloat(rate_usd))),
+                                                                    }
+                                                                }: '',
+                                                                //total_usd: Math.ceil(valueArr[1] * valueArr[0]['details'][valueArr[2]]['rate_usd'])
+                                                            }
+                                                        }
+                                                        //blocksInfoArr.concat(blocksInfoObj);
+                                                        redisClient.setex(blocksInfoRedisKey, 3600, JSON.stringify(blocksInfoArr.concat(blocksInfoObj).sort((a, b) => { return a.block - b.block}).filter((blocksInfoFilObj, index, self) => index === self.findIndex(t => t.block === blocksInfoFilObj.block))));
+                                                    }, 600)
                                                 }
                                             })
-
-                                            const blocksInfoObj = {
-                                                block: blocksObj.height,
-                                                block_hash: blocksObj.hash,
-                                                timestamp: blocksObj.time,
-                                                omni_tx_count: blocksObj.tx.length,
-                                                value: {
-                                                    ...valueArr[0],
-                                                    [details.value_usd_rounded]: Math.ceil((valueArr[1] * rate_usd)),
-                                                    [details.volume]: valueArr[1],
-                                                    total_usd: Math.ceil((valueArr[1] * rate_usd)),
-                                                }
-                                            }
-
-                                            setTimeout(() => {
-                                                redisClient.setex(blocksInfoRedisKey, 3600, JSON.stringify(blocksInfoArr.concat(blocksInfoObj)));
-                                            }, 600)
                                         }
                                     })
+                                    
                                 }
                             })
                         }
                     }
                 })
-                
                 console.log('blocksInfoArr', blocksInfoArr);
             }
         })
--- api/routes/address.js
+++ api/routes/address.js
@@ -1,35 +1,78 @@
-const addressRouter = require('express').Router();
+const express = require('express');
+const addressRouter = express.Router();
 const omniClient = require('../ltc_client');
 
 
-addressRouter.get('/search/:address', (req, res) => {
+addressRouter.post('/search', (req, res) => {
 
-    const { address } = req.params;
+    const { address } = req.query;
 
-    const searchAddressArr = [];
+    const searchBalanceArr = [];
 
     omniClient.cmd('tl_getallbalancesforaddress', address, (err, addressData) => {
         
-        if(addressData) {
-
-            searchAddressArr.push(addressData);
+        if(addressData && addressData.length > 0) {
 
             addressData.map((addressDataProp, index) => {
 
-                omniClient.cmd('tl_getallbalancesforid', addressDataProp.propertyid, (err, propertyData) => {
+                const propertyId = addressDataProp.propertyid;
+
+                omniClient.cmd('tl_getproperty', propertyId, (err, propertyData) => {
 
                     if(propertyData) {
 
-                        searchAddressArr.push(propertyData);
+                        const creationTxId = propertyData.creationtxid;
+
+                        omniClient.cmd('tl_gettransaction', creationTxId, (err, transaction) => {
+
+                            if(transaction) {
+                                
+                                var balance = {
+                                    divisible: transaction.divisible,
+                                    frozen: "",
+                                    id: propertyId.toString(),
+                                    pendingneg: "",
+                                    pendingpos: "",
+                                    propertyinfo: {
+                                        ...transaction,
+                                        category: propertyData.category,
+                                        creationtxid: propertyData.creationtxid,
+                                        ecosystem: "",
+                                        fixedissuance: propertyData.fixedissuance,
+                                        flags: {},
+                                        freezingenabled: "",
+                                        issuer: propertyData.issuer,
+                                        managedissuance: "",
+                                        name: propertyData.name,
+                                        rdata: "",
+                                        registered: "",
+                                        subcategory: propertyData.subcategory,
+                                        totaltokens: propertyData.totaltokens,
+                                        url: propertyData.url,
+                                    },
+                                    reserved: addressDataProp.reserve,
+                                    symbol: `LC${propertyId}`,
+                                    value: addressDataProp.balance
+                                }
+
+                                searchBalanceArr.push(balance);
+                            }
+                        })
                     }
                 })
             })
 
             setTimeout(() => {
                 res.json({
-                    isData: true,
-                    address,
-                    data: searchAddressArr,
+                    data: {
+                        address: {
+                            balance: searchBalanceArr
+                        },
+                        asset: [],
+                        tx: {}
+                    },
+                    query: address,
+                    status: 200
                 })
             }, 600)
         }
